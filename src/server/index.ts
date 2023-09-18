2d85186a485a2c43fbe7ff649ff4641a7a3731e8
israel mailman <80362556+likefools@users.noreply.github.com>
2023-09-19 01:47:12 +0300

    Creating a folder with CLASS user

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { api } from "./api";
 
 const app = express();
 
+app.use(api);
+
+app.get("/api/hi", (req, res) => res.send("Hello"));
 app.listen(3002, () => console.log("Server started"));
